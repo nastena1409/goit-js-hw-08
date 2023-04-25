95c94c2aa81511679dcd01e859aebe8708beca1b
nastena1409 <nastena1409@gmail.com>
2023-04-25 14:18:59 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,43 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+function createGalleryItems(galleryItems) {
+    
+    return galleryItems
+        .map(({ preview, original, description }) => {
+        return `
+    <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+    `
+    }).join('');  
+}
+
+const galleryList = document.querySelector('.gallery');
+const galleryEl = createGalleryItems(galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', galleryEl);
+
+//galleryList.addEventListener('click', onGalleryListClick);
+
+//function onGalleryListClick(event) {
+
+    //if (event.target.nodeName !== 'IMG') {
+      //  return;
+    //}
+
+    //event.preventDefault();
+//}
+
+    new SimpleLightbox('.gallery a',
+        {
+            captionDelay: 250,
+            captionsData: "alt",
+        });
 
 console.log(galleryItems);
