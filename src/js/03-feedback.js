fdefac071415eb4d4463d9ce058e2a26df29cd4f
nastena1409 <nastena1409@gmail.com>
2023-04-27 01:21:22 +0200

    update task 8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('input');
+const messageInput = document.querySelector('textarea');
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = {};
+form.addEventListener('input', onInputForm);
+form.addEventListener('submit', onSubmitForm);
+
+
+// Перевірка наявності даних у сховищі
+const savedData = localStorage.getItem(STORAGE_KEY);
+
+if (savedData) {
+    formData = JSON.parse(savedData);
+    emailInput.value = formData.email || "";
+    messageInput.value = formData.message || "";
+} 
+function onInputForm(e) {
+    formData[e.target.name] = e.target.value 
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onSubmitForm(e) {
+    if (!emailInput.value || !messageInput.value) {
+        return;
+    }
+    e.preventDefault();
+    form.reset();
+    localStorage.removeItem(STORAGE_KEY);
+
+  console.log(formData);
+}
+
+
 
