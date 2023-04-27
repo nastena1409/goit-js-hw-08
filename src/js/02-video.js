2df3fccf3f2dfe03f659eaa71875dc6e642e9ec8
nastena1409 <nastena1409@gmail.com>
2023-04-27 12:43:20 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,8 @@
 import Player from '@vimeo/player'
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
+//const iframe = document.querySelector('iframe');
+const player = new Player('vimeo-player');
 
 const STORAGE_KEY = "videoplayer-current-time";
 const currentTime = localStorage.getItem(STORAGE_KEY);
@@ -14,6 +14,7 @@ if (currentTime) {
 const onPlay = function (data) {
     
     localStorage.setItem(STORAGE_KEY, data.seconds);
-};
     
+};
+    console.log(data)
     player.on('timeupdate', throttle(onPlay, 1000))
